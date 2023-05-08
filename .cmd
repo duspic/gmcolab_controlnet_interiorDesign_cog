7d9bcbf24e7dd60f6be1719ff0189d66be65fbb1
Brett <brettdupree@gmail.com>
2023-05-08 06:49:02 +0000

    ..

---
+++ .cmd
@@ -0,0 +1,6 @@
+cog predict \
+  -i image="https://storage.googleapis.com/remakeable_public/KitchenSink/testmewater1024.png" \
+  -i prompt="a nalgene water sitting on a picnic table overlooking the ocean" \
+  -i a_prompt="" \
+  -i n_prompt="" \
+  -i image_resolution=768
